df926781b16222f58e683925698e8ca996a876da
Gabrielly <gabriellycs@al.insper.edu.br>
2024-09-12 19:04:49 -0300

    testando crc

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 from aplicacao import *
 from enlace import enlace
+import crcmod
 
-#getData = enlace.getData()
+# Definindo a função de CRC-16 (Modbus, por exemplo)
 
+def calcula_crc(payload):
+    crc16 = crcmod.predefined.mkCrcFun('modbus')
+    crc_int = crc16(payload)
+    crc = crc_int.to_bytes(2, byteorder='big')
+    crc1 = crc16[:len(crc)//2]
+    crc2 = crc16[(len(crc)//2)+1:]
+    return crc1, crc2
+    
 
 def fragmenta(mensagem: bytes):
     tamanho = len(mensagem)
@@ -46,22 +55,23 @@ def make_pack(fragmentos : list):
 
         head[0] = len(fragmentos)  # Número total de fragmentos (máximo 255)
         head[1] = i + 1  # Índice do fragmento (começa do 1)
-        
-        #------------------------- erro de tamanho payload ------------------#
         head[2] = len(fragmento)  # Tamanho do fragmento (máximo 255)
-        # head[2] = 2  # Tamanho do fragmento (máximo 255)
+        head[3] = 0  # indica se houve erro  [0 sem erro, 1 para erro]
         
-        
-        head[3] = 0  # Outro campo, ajuste conforme necessário
-
         payload = fragmento  # O fragmento como payload
+        
+        #--------------- CRC -----------------------#
+        head[4], head[5] = calcula_crc(payload)
+        #-------------------------------------------#
+        
+        
         pacote = bytearray(head) + bytearray(payload) + eop  # Concatenar tudo
         
         pacotes.append(pacote)  # Adicionar o pacote à lista
     
     return pacotes
 #---------------------SERVER-------------------------
-def verifica_pack(head, eop, numero_esperado):
+def verifica_pack(head, payload, eop, numero_esperado):
     qtd_pacotes = head[0]
     numero_pacote = head[1]
     if numero_esperado != numero_pacote:
@@ -70,6 +80,13 @@ def verifica_pack(head, eop, numero_esperado):
     if eop != b'\x46\x49\x4D':
         print("ERRO -- tamanho do payload diferente")
         return False
+    
+    #-----------CRC-------------#
+    crc1, crc2 = calcula_crc(payload)
+    if (crc1 != head[4]) or (crc2 != head[5]):
+        print("Erro de CRC")
+        return False
+    #--------------------------#
     return True
         
 def make_pack_server(verifica):
